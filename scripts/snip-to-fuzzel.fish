0cf8c7fbd11428725e0fefdfafba50028b16f477
Tomas Torday <tomas.torday@gmail.com>
2026-09-20 10:20:08 +0200

    snip to fuzzel refactor

--- scripts/snip-to-fuzzel.fish
+++ scripts/snip-to-fuzzel.fish
@@ -8,21 +8,28 @@ if not test -d "$snippet_dir"
 end
 
 # --base-directory forces fd to run inside $snippet_dir and output relative paths
-# outputs eg, lua/some_snippet.lua
-set -l relative_file (fd --type f --base-directory "$snippet_dir" | fuzzel --dmenu -w 60 --placeholder='Alt+1 Copy to Clipboard' --prompt="Snippet ❯ ")
+set -l relative_file (fd --type f --base-directory "$snippet_dir" | fuzzel --dmenu -w 60 --placeholder='Alt+1 Copy | Alt+2 Paste' --prompt="Snippet ❯ ")
 set -l exit_status $status
 
-if contains $exit_status 0 10
-    if test -n "$relative_file"
-        set -l full_path "$snippet_dir/$relative_file"
-        switch $exit_status
-            case 0
-                foot --app-id foot-snip -e nvim "$full_path"
-            case 10
-                wl-copy <"$full_path"
+if contains $exit_status 0 10 11; and test -n "$relative_file"
+    set -l full_path "$snippet_dir/$relative_file"
+
+    switch $exit_status
+        case 0
+            foot --app-id foot-snip -e nvim "$full_path"
+
+        case 10 11
+            wl-copy <"$full_path"
+
+            if test $exit_status -eq 10
                 notify-send "Snippet Copied" "$relative_file" -i edit-copy
-
-        end
+            else
+                # Virtual press Ctrl+V
+                sleep 0.15
+                if not wtype -d 10 -M ctrl -k v -m ctrl >/dev/null 2>&1
+                    notify-send "wtype failed" "Snippet copied to clipboard:\n$relative_file" -i edit-copy
+                    return 1
+                end
+            end
     end
-
 end
